a6315c821a7f4b659f025fe6fc59084b040f086d
正途兼是道 <11112222@qq.com>
2020-03-17 13:51:26 +0800

    自动创建文件夹

--- vedio2image.py
+++ vedio2image.py
@@ -1,4 +1,9 @@
+#coding:utf-8
 import cv2
+import os
+imgDir = './image/'
+if not os.path.exists(imgDir):
+	os.mkdir(imgDir)
 vc=cv2.VideoCapture('vedio.mp4')
 c=1
 if vc.isOpened():
@@ -7,7 +12,7 @@ else:
 	rval=False
 while rval:
 	rval,frame=vc.read()
-	cv2.imwrite('./image/'+str(c-1)+'.jpg',frame)
+	cv2.imwrite(imgDir+str(c-1)+'.jpg',frame)
 	c=c+1
 	cv2.waitKey(1)
 vc.release()
